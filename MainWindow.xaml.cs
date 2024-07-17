468c711b19445f8ccbb7038c809576a0ba664bf8
sergemanzon <44638434+sergemanzon@users.noreply.github.com>
2024-07-17 23:18:09 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory_Game_v3_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            build();
+        }
+
+         private void build() {
+        //   < Grid Margin = "10" >
+        //    < Grid.Background >
+        //        < ImageBrush ImageSource = "Images/m3_login_he.png" />
+        //    </ Grid.Background >
+        //    < Button Click = "BtnClick" />
+
+        //</ Grid >
+
+        Grid grid =new Grid();  
+            grid.Margin=new Thickness(10);  
+            var uri = new Uri("pack://application:,,,/Images/" + "Images/m3_login_he.png");
+            var bitmap = new BitmapImage(uri);
+            grid.Background = new ImageBrush(bitmap);
+
+            Button btn = new Button();
+            btn.Click += BtnClick;
+
+
+
+    
+
+        }
+         
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
